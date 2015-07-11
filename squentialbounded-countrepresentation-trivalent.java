a9b00e7eb21f19913a89e169b325510f42dd7a2c
Dinesh Maharjan <httpdeveloper@gmail.com>
2015-07-11 21:51:32 +0545

    Optimize code

--- squentialbounded-countrepresentation-trivalent.java
+++ squentialbounded-countrepresentation-trivalent.java
@@ -46,10 +46,10 @@ class Mumtest {
         int count = 0;
 
 
-        for (int rupee20 = 0; rupee20 <= numRupees; rupee20++) {
-            for (int rupee10 = 0; rupee10 <= numRupees; rupee10++) {
-                for (int rupee5 = 0; rupee5 <= numRupees; rupee5++) {
-                    for (int rupee2 = 0; rupee2 <= numRupees; rupee2++) {
+        for (int rupee20 = 0; rupee20 <= numRupees/20; rupee20++) {
+            for (int rupee10 = 0; rupee10 <= numRupees/10; rupee10++) {
+                for (int rupee5 = 0; rupee5 <= numRupees/5; rupee5++) {
+                    for (int rupee2 = 0; rupee2 <= numRupees/2; rupee2++) {
                         for (int rupee1 = 0;  rupee1 <= numRupees; rupee1++) {
                             if ( ( (20 * rupee20) + (10 * rupee10) + (5 * rupee5) + (2 * rupee2) + (1 * rupee1)) == numRupees) {
                                 count++;
